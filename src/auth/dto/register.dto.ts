9881799e2cd0d10b4b029505d21acfafa5b902c1
vicc <carrcelt@gmail.com>
2023-11-11 18:57:11 +0100

    Primer commit

---
+++ src/auth/dto/register.dto.ts
@@ -0,0 +1,14 @@
+import { IsEmail, MinLength, IsString } from 'class-validator';
+
+
+export class RegisterDto {
+
+    @IsEmail()
+    email: string;
+
+    @IsString()
+    name: string;
+
+    @MinLength(6)
+    password: string; 
+}
